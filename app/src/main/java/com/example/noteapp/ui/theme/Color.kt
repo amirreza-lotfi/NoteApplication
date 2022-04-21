10019ba45b2741d3b5c3e300af02dc4672a75dff
amirreza <amirreza.lotfi79@gmail.com>
2022-04-22 02:00:57 +0430

    version 2.0.0

--- app/src/main/java/com/example/noteapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/noteapp/ui/theme/Color.kt
@@ -2,11 +2,6 @@ package com.example.noteapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val DarkGray = Color(0xFF202020)
 val LightBlue = Color(0xFFD7E8DE)
 
